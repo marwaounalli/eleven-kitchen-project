9ae10dd29f175e7a615987636dde86accd0683f9
marwa <mounalli@eleven-labs.com>
2023-11-07 17:17:02 +0100

    User and JWT auth

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api', name: 'api_')]
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'register', methods: 'post')]
+    public function register(
+        EntityManagerInterface $entityManager,
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher
+    ): JsonResponse {
+        $decoded = json_decode($request->getContent());
+        $email = $decoded->email;
+        $plaintextPassword = $decoded->password;
+
+        $user = new User();
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        $user->setEmail($email);
+        $user->setUsername($email);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_CREATED);
+    }
+}
